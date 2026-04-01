95e3ef64c2e0bf496a3a49e48144cfcd4ec7e79d
Vedant Ingale <ingalevedant044@gmail.com>
2026-04-01 17:54:20 +0530

    Create Examsarenear.js
     Write a JavaScript to display the message ‘Exams are near, have you started preparing 
    for?’ (user alert box ) and Accept any two numbers from the user and display the 
    addition of two numbers .(Use Prompt and confirm box)

---
+++ Examsarenear.js
@@ -0,0 +1,8 @@
+alert("Exams are near, have you started preparing?");
+
+var a = parseInt(prompt("Enter first number"));
+var b = parseInt(prompt("Enter second number"));
+
+if(confirm("Do you want addition?")){
+    alert("Sum = " + (a+b));
+}
